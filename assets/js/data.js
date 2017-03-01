ee22bae942b476d4c52da93c527c449403bfb8d9
Jef King <jefkin@microsoft.com>
2017-02-28 20:01:27 -0800

    try this

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,13 +1,13 @@
 ﻿[
     {
         "Text": "HTTP -> SQL Database",
-        "TemplateUrl":"https://raw.githubusercontent.com/AzureBytes/functionsstorsql/master/deploy/azuredeploy.json",
+        "TemplateUrl":"http://raw.githubusercontent.com/AzureBytes/functionsstorsql/master/deploy/azuredeploy.json",
         "Repository":"https://github.com/AzureBytes/functionsstorsql/tree/master/src/WebToDatabase"
     },
     {
 
         "Text": "Image Resize",
-        "TemplateUrl":"https://raw.githubusercontent.com/jefking/FunctionLibrary/master/library/deployment.json",
+        "TemplateUrl":"http://raw.githubusercontent.com/jefking/FunctionLibrary/master/library/deployment.json",
         "Repository":"https://github.com/jefking/FunctionLibrary/tree/master/library/src/image-resize-csharp"
     }
 ]
