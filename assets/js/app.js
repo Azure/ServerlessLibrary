48385ad6fb022197ca00ef77e793031bf0ff6c32
Jef King <jefkin@microsoft.com>
2017-02-28 20:10:02 -0800

    method

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,12 @@ var libraryApp = angular.module('libraryApp', []);
 
 libraryApp.controller('library', ['$scope', '$http', function ($scope, $http) {
     $http.get('assets/js/data.js').success(function (data) {
-        $scope.items = data;    
+        $scope.items = data;
+
+         angular.forEach($scope.items, function (item) {
+            item.TemplateUri = function() {
+                return encodeURI(item.TemplateUrl);
+            };
+        });
     });
 }]);
