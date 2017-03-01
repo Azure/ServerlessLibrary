a1c2b8a930325c152e9a1aaa427ba7279609184b
Jef King <jefkin@microsoft.com>
2017-02-28 20:06:47 -0800

    action jackson

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,13 +1,13 @@
 ﻿[
     {
         "Text": "HTTP -> SQL Database",
-        "TemplateUrl":"http://raw.githubusercontent.com/AzureBytes/functionsstorsql/master/deploy/azuredeploy.json",
+        "TemplateUrl":"https://raw.githubusercontent.com/AzureBytes/functionsstorsql/master/deploy/azuredeploy.json",
         "Repository":"https://github.com/AzureBytes/functionsstorsql/tree/master/src/WebToDatabase"
     },
     {
 
         "Text": "Image Resize",
-        "TemplateUrl":"http://raw.githubusercontent.com/jefking/FunctionLibrary/master/library/deployment.json",
+        "TemplateUrl":"https://raw.githubusercontent.com/jefking/FunctionLibrary/master/library/deployment.json",
         "Repository":"https://github.com/jefking/FunctionLibrary/tree/master/library/src/image-resize-csharp"
     }
 ]
