9fd1fcea8879b5c7549faac06ec01c84f2bd73f9
Jef King <jefkin@microsoft.com>
2017-02-28 19:22:52 -0800

    2 quick deploys

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,5 +4,11 @@
         "Text": "HTTP -> SQL Database",
         "TemplateUrl":"https://raw.githubusercontent.com/AzureBytes/functionsstorsql/master/deploy/azuredeploy.json",
         "Repository":"https://github.com/AzureBytes/functionsstorsql/tree/master/src/WebToDatabase",
+    },
+    {
+
+        "Text": "Image Resize",
+        "TemplateUrl":"https://raw.githubusercontent.com/jefking/FunctionLibrary/master/library/deployment.json",
+        "Repository":"https://github.com/jefking/FunctionLibrary/tree/master/library/src/image-resize-csharp",
     }
 ]
