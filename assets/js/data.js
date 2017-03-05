f3f038373b7468e3ce4e942b44e313a8b8b73294
Anthony Chu <anthony@anthonychu.ca>
2017-03-05 00:23:23 -0800

    Add slack user change alerts app

--- assets/js/data.js
+++ assets/js/data.js
@@ -9,5 +9,11 @@
         "Text": "Image -> Resize -> Blob",
         "TemplateUrl": "https://raw.githubusercontent.com/jefking/fl-image-resize/master/deploy/azuredeploy.json",
         "Repository": "https://github.com/jefking/fl-image-resize/tree/master/src/ImageResize"
+    },
+    {
+
+        "Text": "Slack -> User Change Alerts",
+        "TemplateUrl": "https://raw.githubusercontent.com/anthonychu/slack-user-change-alerts/master/azuredeploy.json",
+        "Repository": "https://github.com/anthonychu/slack-user-change-alerts/tree/master/src/SlackUserChangeAlerts.Function"
     }
 ]
