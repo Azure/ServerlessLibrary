20027717b97020ef126ab1e536e02f44db9fa2c9
Jef King <jefkin@microsoft.com>
2017-03-06 16:45:51 -0800

    try this

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,15 +5,18 @@
         "Repository": "https://github.com/AzureBytes/functionsstorsql/tree/master/src/WebToDatabase"
     },
     {
-
         "Text": "Image Resize -> Blob",
         "TemplateUrl": "https://raw.githubusercontent.com/jefking/fl-image-resize/master/deploy/azuredeploy.json",
         "Repository": "https://github.com/jefking/fl-image-resize/tree/master/src/ImageResize"
     },
     {
-
         "Text": "Slack -> User Change Alerts",
         "TemplateUrl": "https://raw.githubusercontent.com/anthonychu/slack-user-change-alerts/master/azuredeploy.json",
+        "Repository": "https://github.com/anthonychu/slack-user-change-alerts/tree/master/src/SlackUserChangeAlerts.Function"
+    },
+    {
+        "Text": "Generic Test Deploy",
+        "TemplateUrl": "https://raw.githubusercontent.com/jefking/FunctionLibrary/master/function-deploy.json",
         "Repository": "https://github.com/anthonychu/slack-user-change-alerts/tree/master/src/SlackUserChangeAlerts.Function"
     }
 ]
