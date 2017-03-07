1f76a06ab802501faf19f0d9d3b101e0367432f3
Jef King <jefkin@microsoft.com>
2017-03-06 16:26:10 -0800

    new empty function

--- assets/js/data.js
+++ assets/js/data.js
@@ -15,5 +15,11 @@
         "Text": "Slack -> User Change Alerts",
         "TemplateUrl": "https://raw.githubusercontent.com/anthonychu/slack-user-change-alerts/master/azuredeploy.json",
         "Repository": "https://github.com/anthonychu/slack-user-change-alerts/tree/master/src/SlackUserChangeAlerts.Function"
+    },
+    {
+
+        "Text": "Convert CSV -> JSON",
+        "TemplateUrl": "https://raw.githubusercontent.com/jefking/fn-csv2json/master/azuredeploy.json",
+        "Repository": "https://github.com/jefking/fn-csv2json/tree/master/src/CsvToJson"
     }
 ]
