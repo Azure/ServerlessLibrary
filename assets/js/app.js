8f73cc02d87be55b0297b2f613c18de9e589bd1f
Jef King <jefkin@microsoft.com>
2017-04-26 15:10:09 -0700

    deeper

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,21 @@ libraryApp.controller('library', ['$scope', '$http', function ($scope, $http) {
     $http.get('assets/js/data.js').success(function (data) {
         $scope.items = data;
 
-         angular.forEach($scope.items, function (item) {
-            item.TemplateUri = function() {
+        angular.forEach($scope.items, function (item) {
+            item.TemplateUri = function () {
                 return encodeURIComponent(item.template);
             };
         });
+
+        $scope.columns = columnize(data, 3);
+        function columnize(input, cols) {
+            var arr = [];
+            for (i = 0; i < input.length; i++) {
+                var colIdx = i % cols;
+                arr[colIdx] = arr[colIdx] || [];
+                arr[colIdx].push(input[i]);
+            }
+            return arr;
+        }
     });
 }]);
