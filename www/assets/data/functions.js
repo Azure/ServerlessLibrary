ade1e51b1d4ef5c7669692ed9bb044c654b02600
Jef King <jefkin@microsoft.com>
2017-05-17 17:39:05 -0700

    remore

--- www/assets/data/functions.js
+++ www/assets/data/functions.js
@@ -58,11 +58,5 @@
         "template": "https://raw.githubusercontent.com/jefking/FunctionLibrary/master/azuredeploy.json",
         "repository": "https://github.com/jefking/FunctionLibrary",
         "description": "An open source set of common use cases for Azure Functions that are ready to deploy!"
-    },
-    {
-        "title": "MSCS: Data Processing Pipeline",
-        "template": "https://raw.githubusercontent.com/Missing-Children-Society-Canada/infrastructure/master/functions.json",
-        "repository": "https://github.com/Missing-Children-Society-Canada/messaging",
-        "description": "Data processing pipeline broken into microservices and run on Azure Functions. Stages include: Filtering, Augmenting, Storing."
     }
 ]
