1d235e3ae04ef0711ccd03c4d3a2245118658812
Faiz Shaikh <fashaikh@microsoft.com>
2017-06-11 19:12:51 -0700

    Seems like onlly one repo is Add functions ready.

--- www/assets/data/functions.js
+++ www/assets/data/functions.js
@@ -9,22 +9,19 @@
         "title": "PDF Extraction for Search",
         "template": "https://raw.githubusercontent.com/m-gagne/PDF2AzSearch/master/azuredeploy.json",
         "repository": "https://github.com/m-gagne/PDF2AzSearch",
-        "description": "This Azure Function binds to an Azure Storage container to process PDF files, extract metadata (using Regular Expressions), stores the result in DocumentDB (extracted text + captured metadata) which can then be used by Azure Search",
-        "addTemplate": "https://raw.githubusercontent.com/fashaikh/PDF2AzSearch/master/addfunction.json"
+        "description": "This Azure Function binds to an Azure Storage container to process PDF files, extract metadata (using Regular Expressions), stores the result in DocumentDB (extracted text + captured metadata) which can then be used by Azure Search"
     },
     {
         "title": "HTTP: Entity to SQL",
         "template": "https://raw.githubusercontent.com/AzureBytes/functionsstorsql/master/deploy/azuredeploy.json",
         "repository": "https://github.com/AzureBytes/functionsstorsql/tree/master/src/WebToDatabase",
-        "description": "Stores a queue message in SQL Database; uses Entity Framework.",
-        "addTemplate": "https://raw.githubusercontent.com/fashaikh/AzureBytes/functionsstorsql/master/deploy/addfunction.json"
+        "description": "Stores a queue message in SQL Database; uses Entity Framework."
     },
     {
         "title": "Blob: Image Resize",
         "template": "https://raw.githubusercontent.com/jefking/fl-image-resize/master/azuredeploy.json",
         "repository": "https://github.com/jefking/fl-image-resize/tree/master/src/ImageResize",
-        "description": "The easiest way to resize images stored in Blob Storage (on Azure); uses ImageProcessor.",
-        "addTemplate": "https://raw.githubusercontent.com/fashaikh/fl-image-resize/master/addfunction.json"
+        "description": "The easiest way to resize images stored in Blob Storage (on Azure); uses ImageProcessor."
     },
     {
         "title": "Slack: User Change Alerts",
@@ -44,22 +41,19 @@
         "title": "Function: Static Server",
         "template": "https://raw.githubusercontent.com/anthonychu/azure-functions-static-file-server/master/azuredeploy.portal.json",
         "repository": "https://github.com/anthonychu/azure-functions-static-file-server/tree/master/src",
-        "description": "Serves a static website. More details at: http://anthonychu.ca/post/azure-functions-static-file-server",
-        "addTemplate": "https://raw.githubusercontent.com/fashaikh/azure-functions-static-file-server/master/addfunction.json"
+        "description": "Serves a static website. More details at: http://anthonychu.ca/post/azure-functions-static-file-server"
     },
     {
         "title": "Blob: Static Server",
         "template": "https://raw.githubusercontent.com/jefking/fl-default-static/master/azuredeploy.json",
         "repository": "https://github.com/jefking/fl-default-static/tree/master/src",
-        "description": "Defaults page (index.htm) for containers. No need for compute on top of raw storage; if you are running static sites.",
-        "addTemplate": "https://raw.githubusercontent.com/fashaikh/slack-user-change-alerts/master/master/addfunction.json"
+        "description": "Defaults page (index.htm) for containers. No need for compute on top of raw storage; if you are running static sites."
     },
     {
         "title": "Blob: Static Server with CDN",
         "template": "https://raw.githubusercontent.com/jefking/fl-default-static/master/azuredeploy.cdn.json",
         "repository": "https://github.com/jefking/fl-default-static/tree/master/src",
-        "description": "Defaults page (index.htm) for containers. No need for compute on top of raw storage; if you are running static sites. This deployment contains a CDN layered 'over top' of the function.",
-        "addTemplate": "https://raw.githubusercontent.com/fashaikh/slack-user-change-alerts/master/addfunction.json"
+        "description": "Defaults page (index.htm) for containers. No need for compute on top of raw storage; if you are running static sites. This deployment contains a CDN layered 'over top' of the function."
     },
     {
         "title": "Raffle",
