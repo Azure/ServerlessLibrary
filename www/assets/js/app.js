bd093e7a5400104c5e615737cd6ed7fb0b4a095f
Jef King <jefkin@microsoft.com>
2017-06-28 14:38:30 -0700

    binding

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -31,9 +31,9 @@ libraryApp.controller('library', ['$scope', '$http', function ($scope, $http) {
 
 armApp.controller('arm', ['$scope', '$http', function ($scope, $http) {
     $http.get('assets/data/arm-input.js').success(function (data) {
-        $scope.input = data;
+        //$scope.input = data;
         // $scope.output = JSON.stringify(data);
-        $scope.current = $scope.input.replace('{{HALP}}', 'JEF KING');
-        $scope.output = JSON.stringify($scope.current);
+        //$scope.current = data.replace('{{HALP}}', 'JEF KING');
+        $scope.output = JSON.stringify(data.replace('{{HALP}}', 'JEF KING'));
     });
 }]);
