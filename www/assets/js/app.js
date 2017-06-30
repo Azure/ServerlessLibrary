7e72ed46223de5f16a530ec79e0dd61c6b1e0d5e
Jef King <jefkin@microsoft.com>
2017-06-30 15:02:33 -0700

    css

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -13,9 +13,6 @@ libraryApp.controller('library', ['$scope', '$http', function ($scope, $http) {
             item.AddTemplateUri = function () {
                 return encodeURIComponent(item.addTemplate)
             };
-            item.icon = function () {
-                return item.type == 'function' ? 'flash' : 'tasks';
-            };
         });
 
         $scope.columns = columnize(data, 3);
