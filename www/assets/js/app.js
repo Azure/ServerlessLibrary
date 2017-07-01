db8e5fce434bc82677b0a29ddae58794aa8291df
Jef King <jefkin@microsoft.com>
2017-07-01 16:07:36 -0700

    hacking

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -44,10 +44,10 @@ libraryApp.controller('arm', ['$scope', '$http', function ($scope, $http) {
 
 
     $scope.urlChange = function () {
-        $scope.data.variables.repoUrl = document.getElementById('repo').value;
+        $scope.data.variables.repoUrl = 'THEKING';//document.getElementById('repo').value;
 
         //var editor  = document.getElementById('container');
-        $scope.editor.setValue('JSON.stringify($scope.data, null, 2)');
+        $scope.editor.setValue(JSON.stringify($scope.data, null, 2));
          //var editor = document.getElementById('container');
         // editor.updateOptions({ 'value': JSON.stringify($scope.data, null, 2) });
     };
