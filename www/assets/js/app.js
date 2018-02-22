8410b431a23e66a77f54a0680178166833142ddd
Jef King <jefkin@microsoft.com>
2018-02-22 11:49:48 -0800

    adding in module

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,6 +1,12 @@
 ﻿'use strict';
 
-var libraryApp = angular.module('libraryApp', []);
+var libraryApp = angular.module('libraryApp'
+    , ['ApplicationInsightsModule']
+);
+
+libraryApp.config(function ($routeProvider, $locationProvider, applicationInsightsServiceProvider) {
+    applicationInsightsServiceProvider.configure('ce23aecf-911b-4d57-b023-c7e0b4dafdc8', { appName: 'serverless-library' });
+});
 
 libraryApp.controller('library', ['$scope', '$http', function ($scope, $http) {
     $http.get('assets/data/apps.js').success(function (data) {
