e0461f4d7de038830d74e39661b8b5c48a74c172
Neha Gupta <45184761+msnehagup@users.noreply.github.com>
2019-04-16 10:45:28 -0700

    Updates to LibraryItem data model, items.json (#135)
    * Separate stats-related properties into separate class
    * Add CreatedDate property, removed AuthorType and AuthorTypeDesc
    * Update id, createddate and author in items.json

--- ServerlessLibraryAPI/Models/LibraryItem.cs
+++ ServerlessLibraryAPI/Models/LibraryItem.cs
@@ -3,10 +3,28 @@ using Newtonsoft.Json;
 
 namespace ServerlessLibrary.Models
 {
+    public class LibraryItemWithStats : LibraryItem
+    {
+        [JsonProperty(PropertyName = "totaldownloads", DefaultValueHandling = DefaultValueHandling.Include)]
+        public int TotalDownloads { get;  set; }
+
+        [JsonProperty(PropertyName = "downloadsthismonth", DefaultValueHandling = DefaultValueHandling.Include)]
+        public int DownloadsThisMonth { get; set; }
+
+        [JsonProperty(PropertyName = "downloadsthisweek", DefaultValueHandling = DefaultValueHandling.Include)]
+        public int DownloadsThisWeek { get; set; }
+
+        [JsonProperty(PropertyName = "downloadstoday", DefaultValueHandling = DefaultValueHandling.Include)]
+        public int DownloadsToday { get; set; }
+    }
+
     public class LibraryItem
     {
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "createddate")]
+        public DateTime CreatedDate { get; set; }
 
         [JsonProperty(PropertyName = "title", DefaultValueHandling = DefaultValueHandling.Include)]
         public string Title { get; set; }
@@ -32,36 +50,13 @@ namespace ServerlessLibrary.Models
         [JsonProperty(PropertyName = "author", DefaultValueHandling = DefaultValueHandling.Include)]
         public string Author { get; internal set; }
 
-        [JsonProperty(PropertyName = "authortype", DefaultValueHandling = DefaultValueHandling.Include)]
-        public string AuthorType { get; set; }
-
-        [JsonProperty(PropertyName = "authortypedesc", DefaultValueHandling = DefaultValueHandling.Include)]
-        public string AuthorTypeDesc { get; set; }
-
-        [JsonProperty(PropertyName = "totaldownloads", DefaultValueHandling = DefaultValueHandling.Include)]
-        public int TotalDownloads { get;  set; }
-
-        [JsonProperty(PropertyName = "downloadsthismonth", DefaultValueHandling = DefaultValueHandling.Include)]
-        public int DownloadsThisMonth { get; set; }
-
-        [JsonProperty(PropertyName = "downloadsthisweek", DefaultValueHandling = DefaultValueHandling.Include)]
-        public int DownloadsThisWeek { get; set; }
-
-        [JsonProperty(PropertyName = "downloadstoday", DefaultValueHandling = DefaultValueHandling.Include)]
-        public int DownloadsToday { get; set; }
-        
         [JsonProperty(PropertyName = "runtimeversion", DefaultValueHandling = DefaultValueHandling.Include)]
         public string RuntimeVersion { get; set; }
 
-        public LibraryItem(string title, string template, string repository, string description, string language,
-            string type)
+        internal T ConvertTo<T>()
         {
-            this.Title = title;
-            this.Template = title;
-            this.Repository = repository;
-            this.Description = description;
-            this.Language = language;
-            this.Type = type;
+            var serializedObj = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<T>(serializedObj);
         }
     }
 }
