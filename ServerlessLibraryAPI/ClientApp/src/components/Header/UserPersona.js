1efb41cd6b1e4b22af404d5759d9aa7fd30e6e9d
Neha Gupta <45184761+msnehagup@users.noreply.github.com>
2019-04-19 17:27:17 -0700

    Surface login/logout operations on app header (#139)

---
+++ ServerlessLibraryAPI/ClientApp/src/components/Header/UserPersona.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Persona, PersonaSize, Icon } from "office-ui-fabric-react";
+import { getTheme } from "office-ui-fabric-react";
+
+class UserPersona extends Component {
+  _onRenderInitials() {
+    return <Icon iconName="Contact" />;
+  }
+
+  render() {
+    const theme = getTheme();
+    const personaStyles = {
+      root: {
+        height: "40px",
+        color: theme.palette.white,
+        float: "right",
+        selectors: {
+          ":hover": {
+            selectors: {
+              $primaryText: {
+                color: theme.palette.white
+              }
+            }
+          }
+        }
+      },
+      details: {
+        width: "85px"
+      },
+      primaryText: {
+        color: theme.palette.white
+      }
+    };
+
+    const { loggedIn, user } = this.props;
+    return loggedIn ? (
+      <Persona
+        styles={personaStyles}
+        text={user.displayName}
+        imageUrl={user.avatarUrl}
+        imageAlt={
+          user.fullName && user.fullName !== ""
+            ? user.fullName
+            : user.displayName
+        }
+        size={PersonaSize.size28}
+      />
+    ) : (
+      <Persona
+        styles={personaStyles}
+        text="Guest"
+        imageAlt="Guest"
+        size={PersonaSize.size28}
+        onRenderInitials={this._onRenderInitials}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  loggedIn: state.authentication.loggedIn,
+  user: state.authentication.user
+});
+
+const UserPersonaContainer = connect(mapStateToProps)(UserPersona);
+
+export default UserPersonaContainer;
---
+++ ServerlessLibraryAPI/ClientApp/src/components/LoginDialog/LoginDialog.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { PrimaryButton } from "office-ui-fabric-react";
+
+import "./LoginDialog.scss";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClick() {
+    const currentLocation = encodeURIComponent(window.location);
+    window.location = `/api/user/login?returnUrl=${currentLocation}`;
+  }
+
+  render() {
+    const buttonStyles = {
+      root: {
+        fontSize: "12px",
+        height: "32px",
+        minWidth: "130px",
+        paddingRight: "10px",
+        paddingLeft: "10px",
+        marginBottom: "10px",
+        marginTop: "10px",
+        border: "1px solid",
+        borderRadius: "2px"
+      },
+      label: {
+        fontWeight: "normal"
+      }
+    };
+
+    return (
+      <div className="login-dialog">
+        <PrimaryButton
+          styles={buttonStyles}
+          primary={true}
+          iconProps={{ iconName: "GitHub-17px" }}
+          onClick={this.handleButtonClick}
+        >
+          Sign in with GitHub
+        </PrimaryButton>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Login);
