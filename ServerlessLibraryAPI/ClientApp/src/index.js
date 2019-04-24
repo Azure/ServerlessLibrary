b48e4af87172adc5659e072920c4fbf1cf5690e4
Neha Gupta <nehagup@microsoft.com>
2019-04-23 17:11:37 -0700

    Apply azure themes

--- ServerlessLibraryAPI/ClientApp/src/index.js
+++ ServerlessLibraryAPI/ClientApp/src/index.js
@@ -3,7 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { initializeIcons } from "office-ui-fabric-react";
+import { initializeIcons, Customizer } from "office-ui-fabric-react";
+import { AzureCustomizationsLight } from "@uifabric/azure-themes";
 import configureStore from "./reducers";
 import registerCustomIcons from "./helpers/registerIcons";
 
@@ -13,12 +14,27 @@ const store = configureStore();
 registerCustomIcons();
 initializeIcons();
 const rootElement = document.getElementById("root");
+const customizations = AzureCustomizationsLight; // or alternatively AzureCustomizationsDark
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <AppContainer />
+      <Customizer {...customizations}>
+        <AppContainer />
+      </Customizer>
     </Provider>
   </BrowserRouter>,
   rootElement
 );
+
+// leaving here to see the UI without the using the theme
+// todo - remove after azure themes have been incorporated completely
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Provider store={store}>
+//       <AppContainer />
+//     </Provider>
+//   </BrowserRouter>,
+//   rootElement
+// );
