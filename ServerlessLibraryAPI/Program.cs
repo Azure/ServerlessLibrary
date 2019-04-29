eae42a51e52fac0906a6286a5a5eba508c515f22
Neha Gupta <nehagup@microsoft.com>
2019-04-29 14:44:33 -0700

    Configure Logging Provider for Asp.Net app

--- ServerlessLibraryAPI/Program.cs
+++ ServerlessLibraryAPI/Program.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 
 namespace ServerlessLibrary
 {
@@ -20,6 +15,15 @@ namespace ServerlessLibrary
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseApplicationInsights()
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(
+                    builder =>
+                    {
+                        builder.AddApplicationInsights();
+                        builder.AddFilter<ApplicationInsightsLoggerProvider>("ServerlessLibrary.Program", LogLevel.Information);
+                        builder.AddFilter<ApplicationInsightsLoggerProvider>("ServerlessLibrary.Startup", LogLevel.Information);
+                        builder.AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Information);
+                    }
+                );
     }
 }
