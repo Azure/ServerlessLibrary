509259e5510b2ffb3a0aebc43340d41b30e1b713
Neha Gupta <nehagup@microsoft.com>
2019-04-29 19:59:12 -0700

    Add support for IE 11

--- ServerlessLibraryAPI/ClientApp/src/index.js
+++ ServerlessLibraryAPI/ClientApp/src/index.js
@@ -1,3 +1,7 @@
+// These must be the first lines in src/index.js
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
