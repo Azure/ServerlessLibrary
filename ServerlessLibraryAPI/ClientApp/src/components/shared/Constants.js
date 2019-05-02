f3f7d1c18c91c66b356d71643d09ef0365901da4
Neha Gupta <nehagup@microsoft.com>
2019-05-01 18:01:12 -0700

    Update technologies list and tags

--- ServerlessLibraryAPI/ClientApp/src/components/shared/Constants.js
+++ ServerlessLibraryAPI/ClientApp/src/components/shared/Constants.js
@@ -1,4 +1,18 @@
-export const technologies = ["Functions 2.x", "Functions 1.x", "Logic Apps"];
+export const technologies = [
+  "Functions 2.x",
+  "Functions 1.x",
+  "Logic Apps",
+  "Blob Storage",
+  "Storage Queue",
+  "Cosmos DB",
+  "Azure Active Directory",
+  "Cognitive Services",
+  "App Service",
+  "Key Vault",
+  "SQL Server",
+  "Service Bus Queue",
+  "Event Grid"
+];
 
 export const solutionAreas = [
   "Web API",
